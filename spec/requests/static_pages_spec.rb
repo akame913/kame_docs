b86deaa2a471923a7ae1a12d4946854a92e6d189
Akira <akame913@gmail.com>
2014-07-24 16:36:47 +0900

    Finish full title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,18 +9,23 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Kame Docs') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+
   end
 
   describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About') }
+    it { should have_title(full_title('About')) }
   end
   
 end
